c9c6614502fcb16c2daf8c7396e8afa295dd9436
Jeroen Ingelbrecht <jeroeningelbrecht@gmail.com>
2013-07-11 01:07:09 +0200

    second version of grader.js

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('./restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -44,12 +45,11 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function(domTree, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
+        var present = domTree(checks[ii]).length > 0;
         out[checks[ii]] = present;
     }
     return out;
@@ -65,10 +65,26 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+	.option('--url <html_file>', 'URL to html file' )      
+	.parse(process.argv);
+	
+	if(program.url != undefined) {
+		rest.get(program.url).on('complete', 
+			function(data){
+				$ = cheerio.load(data);
+				var checkJson = checkHtmlFile($, program.checks);
+				var outJson = JSON.stringify(checkJson, null, 4);
+    				console.log(outJson);	
+			});		
+	}
+   	else {
+		$ = cheerioHtmlFile(program.file);
+    		var checkJson = checkHtmlFile($, program.checks);
+		var outJson = JSON.stringify(checkJson, null, 4);
+    		console.log(outJson);
+	}
+    
+    
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
